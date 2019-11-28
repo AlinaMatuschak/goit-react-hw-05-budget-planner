904869d71d2cf06a95b539e455ad24a32230aa68
Alina <alina90565@gmail.com>
2019-11-28 22:30:57 +0200

    Updates

---
+++ precache-manifest.89dc3039001b9399e611e513773037b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51af8c4a6b7767f16c0f270ef4dbf77f",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "f15371754b99c50fbb9f",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.362c8f33.chunk.css"
+  },
+  {
+    "revision": "22c5615343aaa7748e10",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.e82f5d17.chunk.js"
+  },
+  {
+    "revision": "f15371754b99c50fbb9f",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.a5fd8cae.chunk.js"
+  },
+  {
+    "revision": "59e2c19b54620ec7d5dc",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.c85fa7d4.js"
+  }
+]);
